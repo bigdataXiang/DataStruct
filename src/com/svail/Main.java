060e3369ac7eaf8ca44788b4cebf8062e86f047a
timeloveboy <734991033@qq.com>
2016-07-15 00:11:22 +0800

    ~

--- src/com/svail/Main.java
+++ src/com/svail/Main.java
@@ -1,14 +1,20 @@
 package com.svail;
 
 import com.svail.rank_algorithm.InsertionSort;
+import com.svail.rank_algorithm.Sort;
+
 
 public class Main {
 
     public static void main(String[] args) {
         Integer[] array={34,8,64,51,32,21};
-        array[0]=10;
-        System.out.println(array[0]);
-	// write your code here
-        InsertionSort.insertionSort(array);
+        for (int i:array) {
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+        Integer[] arr=InsertionSort.insertionSort(array, Sort.ASC);
+        for (int i:arr) {
+            System.out.print(i+"\t");
+        }
     }
 }
