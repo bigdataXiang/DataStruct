4152d7c6ba86bb062454cbc459185f1eb73d7e69
timeloveboy <734991033@qq.com>
2016-08-30 03:54:00 +0800

    ~

--- src/com/bigdataXiang/find/BalanceNum.java
+++ src/com/bigdataXiang/find/BalanceNum.java
@@ -2,6 +2,9 @@ package com.bigdataXiang.find;
 
 import com.bigdataXiang.util.Log;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by timeloveboy on 16/8/30.
  * 查找 平衡数Index=k
@@ -9,16 +12,30 @@ import com.bigdataXiang.util.Log;
  */
 public class BalanceNum {
     public static int Solute(int[] N){
+        int sum = Sum(N);
+        int leftsum = 0;
+        for (int i = 1; i < N.length - 1; i++) {
+            int left_right = (sum - N[i]) / 2;
+            leftsum += N[i - 1];
+            if (left_right == leftsum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static Integer[] SoluteAll(int[] N) {
+        List<Integer> Result = new LinkedList<Integer>();
         int sum=Sum(N);
         int leftsum=0;
         for(int i=1;i<N.length-1;i++){
             int left_right=(sum-N[i])/2;
             leftsum+=N[i-1];
             if(left_right==leftsum){
-                return i;
+                Result.add(i);
             }
         }
-        return -1;
+        return Result.toArray(new Integer[Result.size()]);
     }
     public static int Sum(int[] N){
         int sum=0;
@@ -29,5 +46,7 @@ public class BalanceNum {
     public static void main(String[] args){
         int[] v={1,2,3,4,5,7,8,9,8,7,5,4,3,2,1};
         Log.v(Solute(v));
+
+        Log.v(SoluteAll(v).toString());
     }
 }
