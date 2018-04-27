f6633294fe2634e0c25a18a83bb1c503e2df66bd
lipeng29 <Abc!014916>
2018-04-27 22:15:18 +0800

    ~

--- leetcode/5.LongestPalindromicSubstring/Solution.java
+++ leetcode/5.LongestPalindromicSubstring/Solution.java
@@ -1,15 +1,27 @@
+
 /**
  * Created by timeloveboy on 17-12-18.
  */
 public class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 2) return null;
-        int start = 0, median = 1;
-
-        for (int i = 0; i < s.charAt(i); i++) {
-
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
         }
-
-        return s.substring(start, median);
+        return s.substring(start, end + 1);
+    }
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
     }
 }
---
+++ leetcode/9PalindromeNumber/Solution.java
@@ -0,0 +1,41 @@
+import com.bigdataXiang.util.Log;
+
+class Solution {
+    public static void main(String[] args) {
+        Log.v(new Solution().isPalindrome(1000030001));
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int i = 1, y = x, number = (int) Math.log10(y) + 1;
+
+        while (i <= number / 2) {
+
+            int a = y / (int) Math.pow(10, number - i * 2 + 1), b = y % 10;
+
+            if (a != b) {
+                return false;
+            }
+            y = (y - a * (int) Math.pow(10, number - i * 2 + 1)) / 10;
+            Log.v(a, b, y);
+
+            if (0 < y && y < 10) {
+                if (number / 2.0 - number / 2 == 0.5) {
+                    if (number / 2.0 == i + 0.5)
+                        return true;
+                    else
+                        return false;
+                } else {
+                    if (number / 2.0 == i)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            i++;
+        }
+        return true;
+    }
+}
